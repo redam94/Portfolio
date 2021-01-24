8ea9eb1d3db96664d9c63d696be100cb16900b01
Matthew Reda <m.reda94@gmail.com>
2021-01-24 15:01:27 -0500

    Added docker-compose files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Navbar} from './client/components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import "./App.css";
+import {
+  Home,
+  Projects,
+  About, 
+  CVD,
+  NotFound,
+  DRP,
+  IMDB
+} from './client/pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="
+        w-full 
+        h-full 
+        bg-gray-100
+        ">
+    <Router>
+      
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route exact path='/projects'>
+            <Projects/>
+          </Route>
+          <Route exact path='/about'>
+            <About/>
+          </Route>
+          <Route exact path='/projects/cat_or_dog_classifier'>
+            <CVD />
+          </Route>
+          <Route exact path='/projects/disaster_response_pipeline'>
+            <DRP/>
+          </Route>
+          <Route exact path='/projects/imdb_project'>
+            <IMDB/>
+          </Route>
+          <Route component={NotFound}/>
+        </Switch>
+    </Router>
+    </main>
   );
 }
 
