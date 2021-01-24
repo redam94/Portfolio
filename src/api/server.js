75caab08fa347173c4a51d0dbe67f2e7b45f39ed
Matthew Reda <m.reda94@gmail.com>
2021-01-24 14:31:20 -0500

    added api definition that serves tf models

---
+++ src/api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.static("assets"));
+
+app.get("/", (requst, response) => {
+    response.sendFile(__dirname+"/views/index.html")
+});
+
+app.get("/transformer", (request, response, next) => {
+    console.log(request)
+    response.sendFile(__dirname+"/assets/transformer/model.json");
+});
+
+app.get("/disaster", (request, response, next) => {
+    console.log(request)
+    response.sendFile(__dirname + "/assets/disaster/model.json");
+});
+
+const listener = app.listen(3999, () => {
+    console.log("Your app is listening on port "+listener.address().port);
+});
